a96ce6f9f5c3fd8cdee5d785ad217d742547aad6
Alex <alterise.job@gmail.com>
2022-06-11 12:38:33 +0300

    add grpc server and client

---
+++ Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using Server;
+
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7298", new GrpcChannelOptions { HttpHandler = httpHandler });
+var client = new Greeter.GreeterClient(channel);
+
+var input = new HelloRequest{ Name = "Alex"};
+var reply = await client.SayHelloAsync(input);
+
+Console.WriteLine(reply.Message);
+Console.ReadLine();
